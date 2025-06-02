d97accbabb7ab64ca3c0dcc506104fa77553ba9a
edwinarmando.castro <edwinarmando.castro@bbva.com>
2025-06-02 17:31:47 +0200

    feat(exception): Add exceptions control in GlobalExceptionHandler

--- src/main/java/com/ecomerce/pricing/infrastructure/adapters/in/rest/GlobalExceptionHandler.java
+++ src/main/java/com/ecomerce/pricing/infrastructure/adapters/in/rest/GlobalExceptionHandler.java
@@ -1,35 +1,37 @@
 package com.ecomerce.pricing.infrastructure.adapters.in.rest;
 
 import com.ecomerce.pricing.domain.exceptions.DataInitializationException;
+import com.ecomerce.pricing.domain.exceptions.InvalidDateRangeException;
 import com.ecomerce.pricing.domain.exceptions.PriceNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+@Slf4j
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(PriceNotFoundException.class)
     public ResponseEntity<Object> handlePriceNotFound(
             PriceNotFoundException ex,
             WebRequest request
     ) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.NOT_FOUND.value());
-        body.put("error", HttpStatus.NOT_FOUND.getReasonPhrase());
-        body.put("message", ex.getMessage());
-        body.put("path", request.getDescription(false).replace("uri=", ""));
-
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+        log.warn("Resource not found: {}", ex.getMessage());
+        return buildErrorResponse(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND,
+                request
+        );
     }
 
     @ExceptionHandler(Exception.class)
@@ -37,14 +39,26 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             Exception ex,
             WebRequest request
     ) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        body.put("error", "Internal Server Error");
-        body.put("message", "Unexpected error");
-        body.put("path", request.getDescription(false).replace("uri=", ""));
+        log.error("Unexpected error : {}", ex.getMessage(), ex);
+        return buildErrorResponse(
+                "Internal server error. Please contact support.",
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                request
+        );
+    }
 
-        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler({
+            IllegalArgumentException.class,
+            InvalidDateRangeException.class,
+            MethodArgumentNotValidException.class
+    })
+    public ResponseEntity<Object> handleBusinessExceptions(RuntimeException ex, WebRequest request) {
+        log.warn("Business error: {}", ex.getMessage());
+        return buildErrorResponse(
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST,
+                request
+        );
     }
 
     @ExceptionHandler(DataInitializationException.class)
@@ -52,14 +66,40 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
             DataInitializationException ex,
             WebRequest request
     ) {
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        body.put("error", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
-        body.put("message", ex.getMessage());
-        body.put("path", request.getDescription(false).replace("uri=", ""));
-
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+        log.error("Initialization error: {}", ex.getMessage(), ex);
+        return buildErrorResponse(
+                "Internal server error. Please contact support.",
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                request
+        );
     }
 
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Object> handleTypeMismatch(
+            MethodArgumentTypeMismatchException ex,
+            WebRequest request
+    ) {
+        log.warn("Type mismatch: {}", ex.getMessage());
+        return buildErrorResponse(
+                "Invalid value for parameter: " + ex.getName(),
+                HttpStatus.BAD_REQUEST,
+                request
+        );
+    }
+
+
+    private ResponseEntity<Object> buildErrorResponse(
+            String message,
+            HttpStatus status,
+            WebRequest request
+    ) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", status.value());
+        body.put("error", status.getReasonPhrase());
+        body.put("message", message);
+        body.put("path", request.getDescription(false).replace("uri=", ""));
+
+        return new ResponseEntity<>(body, status);
+    }
 }
